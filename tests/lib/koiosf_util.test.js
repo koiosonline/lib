9b8359717d909d13515743c7cafb3f67e1a3e839
Rowin206 <rowin.amsterdam@gmail.com>
2020-10-20 16:01:53 +0200

    Optimised tests

--- tests/lib/koiosf_util.test.js
+++ tests/lib/koiosf_util.test.js
@@ -10,40 +10,38 @@ Object.defineProperty(global.Element.prototype, 'innerText', {
     },
 });
 
-// const htmlLine = <p class="paragraph second-paragraph" id="id7">this is the second paragraph footer</p>;
+const mockBody = `
+<div id="id0" class="header">
+    <p id="id1" class="first-paragraph">this is the first paragraph</p>
+    <p id="id2" class="paragraph second-paragraph">this is the second paragraph</p>
+    <p id="id3" class="paragraph third-paragraph">this is the third paragraph</p>
+    <p id="id4" class="fourth-paragraph">this is the fourth paragraph</p>
+</div>
+<div id="id5" class="footer">
+    <p id="id6" class="first-paragraph">this is the first paragraph</p>
+    <p id="id7" class="paragraph second-paragraph">this is the second paragraph of footer</p>
+    <p id="id8" class="paragraph third-paragraph">this is the third paragraph</p>
+    <p id="id9" class="fourth-paragraph">this is the fourth paragraph</p>
+</div>`;
 
-// describe('getElement', () => {
-//     it('should getElement', () => {
-//         document.body.innerHTML = `
-//         <div id="id0" class="header">
-//             <p id="id1" class="first-paragraph">this is the first paragraph</p>
-//             <p id="id2" class="paragraph second-paragraph">this is the second paragraph</p>
-//             <p id="id3" class="paragraph third-paragraph">this is the third paragraph</p>
-//             <p id="id4" class="fourth-paragraph">this is the fourth paragraph</p>
-//         </div>
-//         <div id="id5" class="footer">
-//             <p id="id6" class="first-paragraph">this is the first paragraph</p>
-//             <p id="id7" class="paragraph second-paragraph">this is the second paragraph of footer</p>
-//             <p id="id8" class="paragraph third-paragraph">this is the third paragraph</p>
-//             <p id="id9" class="fourth-paragraph">this is the fourth paragraph</p>
-//         </div>`;
 
-//         const receivedOutput = getElement('paragraph', 'footer').innerHTML;
-//         const receivedOutputToString = receivedOutput;
+const mockContent = `
+<div id="content">
+    <p class="firstName">    name   </p>
+    <p class="lastName">last name</p>
+</div>`;
 
-//         expect(getElement('paragraph', 'footer')).toBe(<p class="paragraph second-paragraph" id="id7">this is the second paragraph footer</p>);
-//     });
-// });
+
+describe('getElement', () => {
+    it('should getElement', () => {
+        const expectedOutput = '<p id="id7" class="paragraph second-paragraph">this is the second paragraph of footer</p>';
+        document.body.innerHTML = mockBody;
+
+        expect(getElement('second-paragraph', 'footer').outerHTML).toBe(expectedOutput);
+    });
+});
 
 describe('getElementVal', () => {
-    document.body.innerHTML = `
-        <div id="content">
-        <p class="firstName">    name   </p>
-        <p class="domid">domid domid domid</p>
-        <p class="domid2">domid2 domid2 domid2</p>
-        </div>`;
-
-
     it('should return undefined if no parameters are given', () => {
         expect(getElementVal()).toBeUndefined();
     });
@@ -53,25 +51,22 @@ describe('getElementVal', () => {
     });
 
     it('should return the content of paragraph with id "firstName" without any spaces / trimmed', () => {
+        document.body.innerHTML = mockContent;
         expect(getElementVal('firstName')).toBe('name');
     });
 
     it('should return the content of paragraph with class "firstName" without any spaces / trimmed', () => {
+        document.body.innerHTML = mockContent;
         expect(getElementVal('firstName')).toBe('name');
     });
 
     it('should not return the content of paragraph with class "firstName" with all the spaces', () => {
+        document.body.innerHTML = mockContent;
         expect(getElementVal('firstName')).not.toBe('    name   ');
     });
 
     it('should return the content of the first paragraph with class "firstname" and contain "test"', () => {
-        document.body.innerHTML = `
-        <div id="content">
-        <p class="firstName">    name test  </p>
-        <p class="domid">domid domid domid</p>
-        <p class="domid2">domid2 domid2 domid2</p>
-        </div>`;
-
-        expect(getElementVal('firstName')).toContain('test');
+        document.body.innerHTML = mockContent;
+        expect(getElementVal('lastName')).toContain('last');
     });
 });
