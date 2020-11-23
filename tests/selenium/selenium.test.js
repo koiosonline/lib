2f20f77d3467fbcb5c4b8266cb4e84bf608a2ac1
Rowin206 <rowin.amsterdam@gmail.com>
2020-11-23 12:54:04 +0100

    Added last two tests

--- tests/selenium/selenium.test.js
+++ tests/selenium/selenium.test.js
@@ -35,9 +35,9 @@ describe('dashboard', () => {
 
     test('go button click', async () => {
         let driver = await new Builder()
-        .forBrowser('chrome')
-        .setChromeOptions(new chrome.Options().headless())
-        .build();
+            .forBrowser('chrome')
+            .setChromeOptions(new chrome.Options().headless())
+            .build();
 
         try {
             await driver.get("https://www.koios.online/newviewer");
@@ -51,32 +51,39 @@ describe('dashboard', () => {
         }
     });
 
-    // test('settings button click', async () => {
+    test('settings button click', async () => {
+        let driver = await new Builder()
+            .forBrowser('chrome')
+            .setChromeOptions(new chrome.Options().headless())
+            .build();
 
-    //     const driver = await new Builder().forBrowser("chrome").build();
-    //     try {
-    //         await driver.get("https://www.koios.online/newviewer");
-    //         await sleep(10000);
-    //         await driver.findElement(By.xpath("/html/body/div[2]/div[9]/div[1]/div[2]/img")).click();
+        try {
+            await driver.get("https://www.koios.online/newviewer");
+            await sleep(8000);
+            await driver.findElement(By.xpath("/html/body/div[2]/div[9]/div[1]/div[2]/img")).click();
 
-    //         let result = await driver.findElement(By.xpath("/html/body/div[7]/div[2]/div[5]/div[2]")).getText();
-    //         expect(result).toBe("Settings");
-    //     } finally {
-    //         driver.close();
-    //     }
-    // });
-    // test('community button click', async () => {
+            let result = await driver.findElement(By.xpath("/html/body/div[7]/div[2]/div[5]/div[2]")).getText();
+            expect(result).toBe("Settings");
+        } finally {
+            driver.close();
+        }
+    });
 
-    //     const driver = await new Builder().forBrowser("chrome").build();
-    //     try {
-    //         await driver.get("https://www.koios.online/newviewer");
-    //         await sleep(10000);
-    //         await driver.findElement(By.xpath("/html/body/div[2]/div[10]/div/div[3]/div[1]/div[3]/div")).click();
+    test('community button click', async () => {
+        let driver = await new Builder()
+            .forBrowser('chrome')
+            .setChromeOptions(new chrome.Options().headless())
+            .build();
 
-    //         let result = await driver.findElement(By.xpath("/html/body/div[12]/div[2]/div[2]/div[1]")).getText();
-    //         expect(result).toBe("Leaderboard");
-    //     } finally {
-    //         driver.close();
-    //     }
-    // });
+        try {
+            await driver.get("https://www.koios.online/newviewer");
+            await sleep(8000);
+            await driver.findElement(By.xpath("/html/body/div[2]/div[10]/div/div[3]/div[1]/div[3]/div")).click();
+
+            let result = await driver.findElement(By.xpath("/html/body/div[12]/div[2]/div[2]/div[1]")).getText();
+            expect(result).toBe("Leaderboard");
+        } finally {
+            driver.close();
+        }
+    });
 });
